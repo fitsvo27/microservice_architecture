e7c64da6cadea527acb259b74378de61ca07be89
fitsvo27 <150271502+fitsvo27@users.noreply.github.com>
2023-11-14 15:44:21 +0300

    Update create_db.sql

--- db_scheme/create_db.sql
+++ db_scheme/create_db.sql
@@ -20,12 +20,14 @@ CREATE DATABASE users
     IS_TEMPLATE = False;
 
 CREATE SCHEMA IF NOT EXISTS users_scheme
-    AUTHORIZATION study;
+    AUTHORIZATION postgres;
 
+GRANT ALL ON SCHEMA users_scheme TO postgres;
+GRANT USAGE ON SCHEMA users_scheme TO postgres;
 
-CREATE TABLE IF NOT EXISTS users_scheme."Users"
+CREATE TABLE IF NOT EXISTS users_scheme.users
 (
-    id integer NOT NULL,
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
     login character varying(255) COLLATE pg_catalog."default" NOT NULL,
     lastname character varying(255) COLLATE pg_catalog."default" NOT NULL,
     firstname character varying(255) COLLATE pg_catalog."default" NOT NULL,
@@ -35,23 +37,18 @@ CREATE TABLE IF NOT EXISTS users_scheme."Users"
     avatar_link character varying COLLATE pg_catalog."default",
     email character varying COLLATE pg_catalog."default",
     phone_number character varying COLLATE pg_catalog."default" NOT NULL,
-    deleted bit(1),
+    deleted boolean,
     CONSTRAINT "Users_pkey" PRIMARY KEY (id),
     CONSTRAINT avatar_link_un UNIQUE (avatar_link),
     CONSTRAINT email_un UNIQUE (email),
     CONSTRAINT login UNIQUE (login),
-    CONSTRAINT phone_number_un UNIQUE (phone_number),
-    CONSTRAINT users_image_fkey FOREIGN KEY (id)
-        REFERENCES users_scheme.images_storage (user_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID
+    CONSTRAINT phone_number_un UNIQUE (phone_number)
 )
 
 TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS users_scheme."Users"
-    OWNER to study;
+ALTER TABLE IF EXISTS users_scheme.users
+    OWNER to postgres;
 
 
 CREATE TABLE IF NOT EXISTS users_scheme.friends
@@ -68,7 +65,7 @@ CREATE TABLE IF NOT EXISTS users_scheme.friends
 TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS users_scheme.friends
-    OWNER to study;
+    OWNER to postgres;
 
 CREATE TABLE IF NOT EXISTS users_scheme.images_storage
 (
@@ -76,17 +73,12 @@ CREATE TABLE IF NOT EXISTS users_scheme.images_storage
     s3_link bit varying(255) NOT NULL,
     CONSTRAINT users_pkey PRIMARY KEY (user_id)
         INCLUDE(user_id),
-    CONSTRAINT users_fkey FOREIGN KEY (user_id)
-        REFERENCES users_scheme."Users" (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID
 )
 
 TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS users_scheme.images_storage
-    OWNER to study;
+    OWNER to postgres;
 
 CREATE INDEX IF NOT EXISTS fki_users_fkey
     ON users_scheme.images_storage USING btree
