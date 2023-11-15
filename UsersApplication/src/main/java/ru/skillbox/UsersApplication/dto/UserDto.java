74eec44aa889d05b102238c9694b3b102691db0f
fitsvo <fits.v@mail.ru>
2023-11-15 22:10:42 +0300

    Dto + mapper

--- UsersApplication/src/main/java/ru/skillbox/UsersApplication/dto/UserDto.java
+++ UsersApplication/src/main/java/ru/skillbox/UsersApplication/dto/UserDto.java
@@ -2,7 +2,6 @@ package ru.skillbox.UsersApplication.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.persistence.Column;
 import lombok.Builder;
 import lombok.Data;
 
