675ce55c5725945492547adfd5993782c38bb807
fitsvo <fits.v@mail.ru>
2023-12-14 16:15:08 +0300

    add pk

--- InitDatabase/images_storage.sql
+++ InitDatabase/images_storage.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS images_storage
 (
     user_id bigint NOT NULL,
-    s3_link bit varying(255) NOT NULL
+    s3_link bit varying(255) NOT NULL,
+    CONSTRAINT user_id_pk PRIMARY KEY (user_id)
 );
--- InitDatabase/users.sql
+++ InitDatabase/users.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS "users"
 (
-    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
     login character varying(255),
     lastname character varying(255),
     firstname character varying(255),
@@ -10,5 +10,6 @@ CREATE TABLE IF NOT EXISTS "users"
     avatar_link character varying(255),
     email character varying(255),
     phone_number character varying(255),
-    deleted boolean
+    deleted boolean,
+    CONSTRAINT user_id_pk PRIMARY KEY (id)
 );
